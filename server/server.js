29450591bb346bd3ee8e4c5aac5112104bb816b0
575ml <aiza.espineda@gmail.com>
2018-04-11 15:09:29 +1000

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-var	express    = require('express'),
-	bodyParser = require('body-parser'),
-	{mongoose} = require('./db/mongoose'),
-	{Todo}     = require('./models/todo'),
-	{User}     = require('./models/user'),
-	app 	   = express(),
-	{ObjectID} = require('mongodb');
+const	express    = require('express'),
+		bodyParser = require('body-parser'),
+		{mongoose} = require('./db/mongoose'),
+		{Todo}     = require('./models/todo'),
+		{User}     = require('./models/user'),
+		app 	   = express(),
+		{ObjectID} = require('mongodb'),
+		_ 		   = require('lodash');
 
 const port = process.env.PORT || 5000;
 
@@ -40,7 +41,7 @@ app.get('/todos/:id', (req, res) => {
 	//ID is invalid
 	if (!ObjectID.isValid(id)) {
 		return res.status(404).send();
-		console.log("Invalid ID");
+		// console.log("Invalid ID");
 	}
 
 	Todo.findById(id).then((todo) => {
@@ -66,7 +67,7 @@ app.delete('/todos/:id', (req, res) => {
 		return res.status(404).send();
 		console.log("Invalid ID");
 	}
-	
+
 	Todo.findByIdAndRemove(id).then((todo) => {
 		if(!todo) {
 			console.log(err);
@@ -79,6 +80,32 @@ app.delete('/todos/:id', (req, res) => {
 	});
 });
 
+
+app.patch('/todos/:id', (req, res) => {
+	  var id = req.params.id;
+	  var body = _.pick(req.body, ['text', 'completed']);
+
+	  if (!ObjectID.isValid(id)) {
+	    return res.status(404).send();
+	  }
+
+	  if (_.isBoolean(body.completed) && body.completed) {
+	    body.completedAt = new Date().getTime();
+	  } else {
+	    body.completed = false;
+	    body.completedAt = null;
+	  }
+
+	  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+	    if (!todo) {
+	      return res.status(404).send();
+	    }
+
+	    res.send({todo});
+	  }).catch((e) => {
+	    res.status(400).send();
+	  })
+});
 
 //SERVER
 app.listen(port, () => {
