d3a5421fb5ef85f9d70e3a91ff1760cb95a10c02
575ml <aiza.espineda@gmail.com>
2018-04-13 08:31:57 +1000

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,21 @@
 var env = process.env.NODE_ENV || 'development';
 console.log('env ***********', env);
 
-if (env === 'development') {
-	process.env.PORT = 5000;
-	process.env.DATABASEURL = 'mongodb://localhost/TodoApp';
-} else if (env === 'test') {
-	process.env.PORT = 5000;
-	process.env.DATABASEURL = 'mongodb://localhost/TodoAppTest';
+
+if (env === 'development' || env === 'test') {
+	var config = require('./config.json');
+	var envConfig = config[env];
+
+	Object.keys(envConfig).forEach((key) => {
+		process.env[key] = envConfig[key];
+	});
 }
+
+
+// if (env === 'development') {
+// 	process.env.PORT = 5000;
+// 	process.env.DATABASEURL = 'mongodb://localhost/TodoApp';
+// } else if (env === 'test') {
+// 	process.env.PORT = 5000;
+// 	process.env.DATABASEURL = 'mongodb://localhost/TodoAppTest';
+// }
